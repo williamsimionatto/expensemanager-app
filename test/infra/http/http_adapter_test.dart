81f7b4077f197afb5211b1355de61c07f5b2f3a7
William Simionatto <william.sn0226@gmail.com>
2023-03-29 21:52:12 -0300

    test: ensure HttpAdapter returns ServerError if get returns 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -73,5 +73,12 @@ void main() {
       final future = sut.request(url: url, method: 'get');
       expect(future, throwsA(HttpError.notFound));
     });
+
+    test('Should return ServerError if get returns 500', () async {
+      client.mockGet(500);
+
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }

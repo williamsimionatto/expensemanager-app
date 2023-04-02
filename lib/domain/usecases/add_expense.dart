0751b9df0ec36462a6e795cbbf8d7bb91681fc62
William Simionatto <william.sn0226@gmail.com>
2023-04-02 10:55:12 -0300

    feat: create AddExpense usecase

---
+++ lib/domain/usecases/add_expense.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:expensemanagerapp/domain/entities/expense_entity.dart';
+
+abstract class AddExpense {
+  Future<ExpenseEntity> add(AddExpenseParams params);
+}
+
+class AddExpenseParams extends Equatable {
+  final String description;
+  final double amount;
+  final String date;
+  final int categoryId;
+  final int periodId;
+
+  @override
+  List get props => [description, amount, date, categoryId, periodId];
+
+  const AddExpenseParams({
+    required this.description,
+    required this.amount,
+    required this.date,
+    required this.categoryId,
+    required this.periodId,
+  });
+}
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,3 +1,4 @@
 export './load_expenses.dart';
 export './load_periods.dart';
 export './load_period_categories.dart';
+export './add_expense.dart';

d1617f8b5a18b3a4c19f68091f5dbaa3bab644b4
William Simionatto <william.sn0226@gmail.com>
2023-04-05 18:55:37 -0300

    test: ensure RemoteLoadPeriods throws UnexpectedError if HttpClient returns 400

--- test/data/usecases/load_periods/remote_load_periods_test.dart
+++ test/data/usecases/load_periods/remote_load_periods_test.dart
@@ -1,3 +1,5 @@
+import 'package:expensemanagerapp/data/http/http_error.dart';
+import 'package:expensemanagerapp/domain/helpers/helpers.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -21,5 +23,11 @@ void main() {
   test('Should call HttpClient with correct values', () async {
     await sut.load();
     verify(() => httpClient.request(url: url, method: 'get'));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    httpClient.mockRequestError(HttpError.notFound);
+    final future = sut.load();
+    expect(future, throwsA(DomainError.unexpected));
   });
 }

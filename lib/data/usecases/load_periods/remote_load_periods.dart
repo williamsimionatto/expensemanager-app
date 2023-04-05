eab20b851896325e55efa6c6ea8af6790d200b82
William Simionatto <william.sn0226@gmail.com>
2023-04-05 19:05:01 -0300

    feat: ensure RemoteLoadPeriods return a periods list on 200

--- lib/data/usecases/load_periods/remote_load_periods.dart
+++ lib/data/usecases/load_periods/remote_load_periods.dart
@@ -1,4 +1,6 @@
 import 'package:expensemanagerapp/data/http/http.dart';
+import 'package:expensemanagerapp/data/model/model.dart';
+import 'package:expensemanagerapp/domain/entities/entities.dart';
 import 'package:expensemanagerapp/domain/helpers/helpers.dart';
 
 class RemoteLoadPeriods {
@@ -7,9 +9,14 @@ class RemoteLoadPeriods {
 
   RemoteLoadPeriods({required this.url, required this.httpClient});
 
-  Future<void> load() async {
+  Future<List<PeriodEntity>> load() async {
     try {
-      await httpClient.request(url: url, method: 'get');
+      final httpResponse = await httpClient.request(url: url, method: 'get');
+
+      return httpResponse
+          .map<PeriodEntity>(
+              (json) => RemotePeriodModel.fromJson(json).toEntity())
+          .toList();
     } catch (error) {
       throw DomainError.unexpected;
     }

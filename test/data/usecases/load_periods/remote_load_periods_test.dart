35d354ed6e473f8c6d3b399a057d8a0a72d11091
William Simionatto <william.sn0226@gmail.com>
2023-04-05 19:05:05 -0300

    test: ensure RemoteLoadPeriods return a periods list on 200

--- test/data/usecases/load_periods/remote_load_periods_test.dart
+++ test/data/usecases/load_periods/remote_load_periods_test.dart
@@ -1,23 +1,28 @@
-import 'package:expensemanagerapp/data/http/http_error.dart';
-import 'package:expensemanagerapp/domain/helpers/helpers.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:expensemanagerapp/data/http/http.dart';
 import 'package:expensemanagerapp/data/usecases/usecases.dart';
 
+import 'package:expensemanagerapp/domain/entities/entities.dart';
+import 'package:expensemanagerapp/domain/helpers/helpers.dart';
+
+import '../../../infra/mocks/mocks.dart';
 import '../../mocks/mocks.dart';
 
 void main() {
   late String url;
   late HttpClientSpy httpClient;
   late RemoteLoadPeriods sut;
+  late List<Map> list;
 
   setUp(() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
     sut = RemoteLoadPeriods(url: url, httpClient: httpClient);
-    httpClient.mockRequest([]);
+    list = ApiFactory.makePeriodsList();
+    httpClient.mockRequest(list);
   });
 
   test('Should call HttpClient with correct values', () async {
@@ -35,5 +40,27 @@ void main() {
     httpClient.mockRequestError(HttpError.serverError);
     final future = sut.load();
     expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should return periods on 200', () async {
+    final periods = await sut.load();
+    expect(periods.length, 2);
+
+    expect(periods, [
+      PeriodEntity(
+        id: list[0]['id'],
+        name: list[0]['name'],
+        startDate: list[0]['startDate'],
+        endDate: list[0]['endDate'],
+        budget: list[0]['budget'],
+      ),
+      PeriodEntity(
+        id: list[1]['id'],
+        name: list[1]['name'],
+        startDate: list[1]['startDate'],
+        endDate: list[1]['endDate'],
+        budget: list[1]['budget'],
+      ),
+    ]);
   });
 }

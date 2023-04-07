347dda596d3181414d859eb06639177ae3ec1412
William Simionatto <william.sn0226@gmail.com>
2023-04-07 15:25:36 -0300

    test: ensure MinLengthValidation return error if value is less than min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:expensemanagerapp/presentation/protocols/validation.dart';
@@ -17,6 +18,11 @@ void main() {
 
   test('Should return error if value is null', () {
     final formData = {'any_field': null};
+    expect(sut.validate(formData), ValidationError.invalidField);
+  });
+
+  test('Should return error if value is less than min size', () {
+    final formData = {'any_field': faker.randomGenerator.string(4, min: 1)};
     expect(sut.validate(formData), ValidationError.invalidField);
   });
 }

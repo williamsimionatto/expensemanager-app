361cdc313c33f7fcaf4b16c883be9d2582ff81f1
William Simionatto <william.sn0226@gmail.com>
2023-04-29 11:17:27 -0300

    feat: put button side by side

--- lib/ui/pages/add_expense/components/step2_page.dart
+++ lib/ui/pages/add_expense/components/step2_page.dart
@@ -17,43 +17,38 @@ class Step2Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Builder(
       builder: (context) {
-        return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ListenableProvider(
-                create: (_) => presenter,
-                child: Form(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.1,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: CategoryInput(),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: DescriptionInput(),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: AmountInput(),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: DateInput(),
-                      ),
-                      Column(
-                        children: <Widget>[
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: AddExpenseButton(),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ListenableProvider(
+                  create: (_) => presenter,
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CategoryInput(),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: DescriptionInput(),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: AmountInput(),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: DateInput(),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            IconButton(
                               onPressed: () {
                                 controller.animateToPage(
                                   0,
@@ -61,17 +56,19 @@ class Step2Page extends StatelessWidget {
                                   curve: Curves.easeInOut,
                                 );
                               },
-                              child: const Text('Back'),
+                              icon: const Icon(Icons.arrow_back),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                            const SizedBox(width: 8),
+                            const Expanded(child: AddExpenseButton()),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
